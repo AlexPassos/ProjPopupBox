e1dc2d86196eb0c45497c2e94d339cacc1f4fe99
Alex Passos <alexpassosribeiro@gmail.com>
2021-10-03 15:59:59 -0300

    Primeiro Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjPopupBox';
+
+  cActive: string = ''
+
+  fnAbrir(){
+    this.cActive = 'active';
+  }
+
+  fnFechar(){
+    this.cActive = '';
+  }
 }
